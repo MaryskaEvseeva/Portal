65b23984daacb16d8ec1a6ecc3eba63b92ba3514
Neto test <sysl86@mail.ru>
2023-09-18 21:50:07 +0400

    commit 2

--- src/test/java/test/DeliveryTest.java
+++ src/test/java/test/DeliveryTest.java
@@ -3,9 +3,9 @@ package test;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.conditions.Text;
 import com.codeborne.selenide.conditions.Visible;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import data.DataGenerator;
 import org.openqa.selenium.Keys;
 
@@ -21,6 +21,15 @@ class DeliveryTest {
     @BeforeEach
     void setup() {
         open("http://localhost:9999/");
+    }
+
+    @BeforeAll
+    static void setUpAll(){
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll(){
+        SelenideLogger.removeListener("allure");
     }
 
     @Test
